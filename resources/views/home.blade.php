ad6c4db8b360b397fedca0d730da4849c6a24597
marionaldocabral <marionaldocabral@hotmail.com>
2018-06-20 00:35:45 -0300

    Autenticação

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Painel de Controle</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    Você está logado!
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
